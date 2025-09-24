fe81541097601940d3b754234046eb38aa139b3f
Bilguun-Zorig <bilguun.zorig@yahoo.com>
2025-09-23 21:49:44 -0400

    creating user and testing fullstack

--- client/src/components/UserForm.jsx
+++ client/src/components/UserForm.jsx
@@ -3,17 +3,59 @@ import axios from 'axios';
 
 const UserForm = () => {
 
-    const [message, setMessage] = useState()
+  const [userInfo, setUserInfo] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  });
 
-    useEffect(() => {
-        axios.get("http://localhost:8000/api")
-        .then(res => setMessage(res.data.message))
-        .catch(err => console.log(err))
+
+
+  const onSubmitHandler = e => {
+    e.preventDefault();
+    axios.post("http://localhost:8000/api/user", userInfo)
+      .then(res => {
+        console.log(res);
+        setUserInfo({
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: ""
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  const changeHandler = e => {
+    setUserInfo({
+      ...userInfo, 
+      [e.target.name]: e.target.value
     })
+  }
 
   return (
     <div>
-        <h2>Message from the backend: {message}</h2>
+      <form action="#" onSubmit={onSubmitHandler}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input type="text" name='firstName' onChange={changeHandler} value={userInfo.firstName}/>
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input type="text" name='lastName' onChange={changeHandler} value={userInfo.lastName}/>
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name='email' onChange={changeHandler} value={userInfo.email}/>
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name='password' onChange={changeHandler} value={userInfo.password}/>
+        </div>
+        
+        <input type="submit" />
+      </form>
     </div>
   )
 }
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     firstName: {type: String},
-    lastName: {type: String}
+    lastName: {type: String},
+    email: {type: String},
+    password: {type: String}
 }, {timestamps: true});
 
 module.exports = mongoose.model('User', UserSchema);
